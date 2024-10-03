5221021ef33784c9fd3949217e65beee7f3e9d73
Keseres <157545298+Keseres@users.noreply.github.com>
2024-10-03 17:46:44 +0200

    Add files via upload

---
+++ homeworks/hw0/SochaKarol/lab01.R
@@ -0,0 +1,130 @@
+###########################################
+###    TECHNIKI WIZUALIZACJI DANYCH     ###
+###           LABORATORIUM 1            ###
+###########################################
+
+
+# 0. Prowadzący  -------------------------------------------------------------
+
+# Anna Kozak/Maciej Chrabąszcz/Hubert Ruczyński/Katarzyna Woźnica
+# Kontakt: MS Teams lub mail
+
+
+# 1. Sposób pracy na zajęciach laboratoryjnych -------------------------------
+
+# a) pracujemy w R (większość semestru) i Python
+# b) pracujemy na przygotowanych plikach, które będą na repozytorium przedmiotu
+# c) podczas zajęć prowadzący będzie wprowadzał zagdanienia, a następnie będzie rozwiązywanie zadań w celu utrwalenia wiadomości
+# d) kolejna porcja materiału będzie omawiana jeżeli większość grupy wykona zadane zadanie 
+# e) wszelkie pytania czy to związane z kodem, pracą domową czy kwestie teoretyczne proszę śmiało zgłaszać prowadzącemu 
+
+
+# 2. Materiały ------------------------------------------------------------
+
+# Repozytorium na GitHub
+# https://github.com/kozaka93/2024Z-DataVisualizationTechniques
+
+
+# 3. Jak działa GitHub? ---------------------------------------------------
+
+# Jak zgłosić pracę domową/projekt? (fork, commit, pull request)
+# https://rogerdudler.github.io/git-guide/
+
+
+# 4. Podstawy R - rozgrzewka ----------------------------------------------
+
+data(mtcars)
+head(mtcars, 10)
+tail(mtcars, 4)
+dim(mtcars)
+str(mtcars)
+head(mtcars, 10)
+mtcars[1:5,3:6]
+mtcars[1,1]
+
+
+# Jak wybieramy wiersze (obserwacje) oraz kolumny (zmienne)?
+
+
+# Pierwszy wiersz, pierwsza kolumna?
+mtcars[1,1]
+
+# 10 pierszych wierszy, 2 i 3 kolumna?
+mtcars[1:10,2:3]
+
+# Jak wybieramy kolumny po nazwach? 
+head(mtcars[c('mpg','hp')],10)
+
+
+# Wszystkie wiersze i kolumny w kolejności "am", "wt", "mpg"?
+mtcars[,c("am", "wt", "mpg")]
+
+# Jak wybierać jedną kolumnę?
+mtcars[,'mpg']
+
+# Uwaga na przecinek i wybór kolumn poprzez indeksy
+
+# Pytania
+
+# 1. Wymiar ramki danych
+dim(mtcars)
+
+# 2. Jakie są typy zmiennych?
+str(mtcars)
+
+# 3. Ile jest unikalnych wartości zmiennej "cyl" i jakie to są wartości?
+length(unique(mtcars$cyl))
+
+# 4. Jaka jest średnia wartość zmiennej "drat" dla samochodów
+# o wartości zmiennej "cyl" równej 4?
+mean(mtcars$cyl == 4)
+
+# 5. Jakie są unikalne wartości zmiennej "am" i jaki jest ich rozkład (liczba wystąpień)? 
+table(mtcars$am)
+unique(mtcars$am)
+
+# Prosty wykres
+plot(mtcars$mpg, mtcars$hp)
+barplot(table(mtcars$cyl))
+
+# Zależność "mpg" i "hp" - scatter plot
+
+
+# Zmienna "cyl" - barplot
+
+
+# 5. Zadanie "proton" -----------------------------------------------------
+
+# Utwórz nowy plik .R. Zainstaluj/uruchom pakiet proton:
+# install.packages("proton")
+# library(proton)
+proton()
+head(employees)
+employees[(employees$name=='John' & employees$surname=='Insecure'),]
+proton(action = "login", login="johnins")
+
+proton(action = "login", login="johnins", password="2")
+
+for (pass in top1000passwords) {
+  response <- proton(action = "login", login="johnins", password=pass)
+  if (response == "Success! User is logged in!") {
+    cat(pass)
+    break
+  }
+}
+
+q1w2e3r4t5
+proton(action = "login", login="johnins", password="q1w2e3r4t5")
+
+
+proton(action = "server", host="XYZ")
+
+head(logs[login])
+df <- as.data.frame(unique(logs$login))
+df
+# W pliku zapisuj sposób rozwiązania gry.
+
+
+
+# 6. Zamieszczamy rozwiązanie na repozytorium -----------------------------
+
